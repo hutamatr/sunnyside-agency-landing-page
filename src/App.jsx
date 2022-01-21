c1473ed68c326d0f91305a692a20f5f8a0789d7e
hutamatr <hutamatr@gmail.com>
2022-01-22 01:38:30 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeImg from '../assets/mobile/image-header.jpg';
+
+const Home = () => {
+  return (
+    <section>
+      <img src={HomeImg} alt="" />
+      <img src="" alt="" />
+    </section>
+  );
+};
+
+export default Home;
