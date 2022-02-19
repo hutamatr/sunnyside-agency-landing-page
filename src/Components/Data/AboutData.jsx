e4507346206de1c62f4f0e3e3e7e48e2312ec078
hutamatr <hutamatr@gmail.com>
2022-02-19 15:57:51 +0700

    Edit data & some component

--- src/Components/Data/AboutData.jsx
+++ src/Components/Data/AboutData.jsx
@@ -1,22 +1,22 @@
-import TransformMobile from '../../assets/mobile/image-transform.jpg';
-import StandOutMobile from '../../assets/mobile/image-stand-out.jpg';
-import TransformDekstop from '../../assets/desktop/image-transform.jpg';
-import StandOutDekstop from '../../assets/desktop/image-stand-out.jpg';
+import TransformMobile from "../../assets/mobile/image-transform.jpg";
+import StandOutMobile from "../../assets/mobile/image-stand-out.jpg";
+import TransformDesktop from "../../assets/desktop/image-transform.jpg";
+import StandOutDesktop from "../../assets/desktop/image-stand-out.jpg";
 
 const AboutData = [
   {
     imageMobile: TransformMobile,
-    imageDekstop: TransformDekstop,
-    title: 'Transform your brand',
+    imageDesktop: TransformDesktop,
+    title: "Transform your brand",
     paragraph:
-      'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.',
+      "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
   },
   {
     imageMobile: StandOutMobile,
-    imageDekstop: StandOutDekstop,
-    title: 'Stand out to the right audience',
+    imageDesktop: StandOutDesktop,
+    title: "Stand out to the right audience",
     paragraph:
-      'Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. ',
+      "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. ",
   },
 ];
 
--- src/Components/Main/About.jsx
+++ src/Components/Main/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import AboutData from '../Data/AboutData';
-// import TransformDekstop from '../../assets/desktop/image-transform.jpg';
-// import StandOutDekstop from '../../assets/desktop/image-stand-out.jpg';
+import React from "react";
+import AboutData from "../Data/AboutData";
+// import TransformDesktop from '../../assets/desktop/image-transform.jpg';
+// import StandOutDesktop from '../../assets/desktop/image-stand-out.jpg';
 
 const About = () => {
   // const [imageAbout, setImageAbout] = useState(false);
@@ -12,19 +12,19 @@ const About = () => {
           <div key={i} className="grid md:grid-cols-2">
             <img src={content.imageMobile} alt="" className="md:hidden" />
             <img
-              src={content.imageDekstop}
+              src={content.imageDesktop}
               alt=""
               className="hidden md:block"
             />
             <article
-              className={`flex flex-col items-center justify-center gap-y-6 px-8 py-16 md:py-6 md:pl-10 md:pr-12 md:items-start lg:pr-28 lg:pl-32 lg:gap-y-8  ${
-                i === 0 ? 'md:order-first' : ''
+              className={`flex flex-col items-center justify-center gap-y-6 px-8 py-16 md:items-start md:py-6 md:pl-10 md:pr-12 lg:gap-y-8 lg:pr-28 lg:pl-32  ${
+                i === 0 ? "md:order-first" : ""
               }`}
             >
-              <h1 className="font-fraunces text-center text-3xl text-veryDarkDesaturated md:text-left lg:text-[2.5rem] lg:leading-tight">
+              <h1 className="font-fraunces text-veryDarkDesaturated text-center text-3xl md:text-left lg:text-[2.5rem] lg:leading-tight">
                 {content.title}
               </h1>
-              <p className="text-center text-sm font-extralight leading-relaxed text-darkGrayish md:text-left lg:text-base">
+              <p className="text-darkGrayish text-center text-sm font-extralight leading-relaxed md:text-left lg:text-base">
                 {content.paragraph}
               </p>
               <a href="##" className="box">
